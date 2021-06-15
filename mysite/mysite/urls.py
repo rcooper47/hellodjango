979159c0a322b4dbca4b543e8b4eb08d9f1ee20e
ryan cooper <3RCOOPER8@GMAIL.COM>
2021-06-15 17:34:13 -0400

    Create site

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,3 +1,6 @@
+from django.contrib import admin
+from django.urls import include, path
+
 """mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -17,5 +20,6 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
+    path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
